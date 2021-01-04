18a7eee5c9cb51330a1c9f1c1b015657e38cf247
BlueNebulaDev <76972454+BlueNebulaDev@users.noreply.github.com>
2021-01-04 19:48:07 -0100

    Initial commit

---
+++ main/index.js
@@ -0,0 +1,9 @@
+
+let a1 = require('a');
+let a2 = require('a');
+let b1 = require('b');
+let b2 = require('b');
+
+console.log( "A===A: ", a1 === a2 );
+console.log( "B===B: ", b1 === b2 );
+console.log( "A===B: ", a1 === b1 );
